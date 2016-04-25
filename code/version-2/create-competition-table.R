1353233cc7222ae133daa1fb951f062f931b3ecb
amj2012 <alfredom790@gmail.com>
2016-04-25 08:54:08 -0700

    Update calculations in create-competition-table.R
    Accounted for changes in stats names

--- code/version-2/create-competition-table.R
+++ code/version-2/create-competition-table.R
@@ -64,65 +64,74 @@ rbindCompetitionMatches <- function(match_list) {
   }
   
   #for certain columns, we need to recalculate the values
+  overall[,"Shots.per.90"] <- (overall$Shots/overall$MP)*90
   overall[,"Shot.Accuracy"] <- (overall$Goals + overall$Shot.GK.Stop + overall$Shot.Def.Stop)/
     (overall$Goals + overall$Shot.GK.Stop + overall$Shot.Def.Stop + overall$Shot.Miss)
-  overall[,"Pct.Shots.Pressd"] <- overall[,"Shot.Pressd"]/overall[,"Shots"]
-  overall[,"Shots.per.90"] <- (overall$Shots/overall$MP)*90
-  overall[,"KP.to.Goal"] <- (overall[,"Key.Assists"] + overall[,"Second.Assists"])/overall[,"Key.Passes"]
-  overall[,"BC.to.Goal"] <- overall[,"BC.Scored"]/overall[,"Big.Chances"]
-  overall[,"Big.Chances.per.90"] <- (overall[,"Big.Chances"]/overall$MP)*90
+  overall[,"Pct.Shots.Pressed"] <- overall[,"Shot.Pressed"]/overall[,"Shots"]
   overall[,"Assists.per.90"] <- (overall$Assists/overall$MP)*90
   overall[,"Key.Passes.per.90"] <- (overall[,"Key.Passes"]/overall$MP)*90
-  overall[,"Cross.Comp.Pct"] <- overall[,"Cross.Comp"]/overall[,"Cross.Att"]
-  overall[,"Cross.Att.per.90"] <- (overall[,"Cross.Att"]/overall$MP)*90
-  overall[,"Cross.Att.per.Pass"] <- (overall[,"Cross.Att"]/overall["Pass.Att"])
-  overall[,"Cross.Att.per.opPass"] <- (overall[,"Cross.Att"]/overall["opPass.Att"])
-  overall[,"Launch.Comp.Pct"] <- overall[,"Launch.Comp"]/overall[,"Launch.Att"]
-  overall[,"Launch.Att.per.90"] <- (overall[,"Launch.Att"]/overall$MP)*90
-  overall[,"Launch.Att.per.Pass"] <- (overall[,"Launch.Att"]/overall["Pass.Att"])
-  overall[,"Launch.Att.per.opPass"] <- (overall[,"Launch.Att"]/overall["opPass.Att"])
-  overall[,"Through.Comp.Pct"] <- overall[,"Through.Comp"]/overall[,"Through.Att"]
-  overall[,"Through.Att.per.90"] <- (overall[,"Through.Att"]/overall$MP)*90
-  overall[,"Through.Att.per.Pass"] <- (overall[,"Through.Att"]/overall["Pass.Att"])
-  overall[,"Through.Att.per.opPass"] <- (overall[,"Through.Att"]/overall["opPass.Att"])
-  overall[,"Throw.In.Comp.Pct"] <- overall[,"Throw.In.Comp"]/overall[,"Throw.In.Att"]
-  overall[,"Throw.In.Att.per.90"] <- (overall[,"Throw.In.Att"]/overall$MP)*90
-  overall[,"Throw.In.Att.per.Pass"] <- (overall[,"Throw.In.Att"]/overall["Pass.Att"])
-  overall[,"CK.Effectiveness"] <- overall[,"Corner.Kicks.Completed"]/overall[,"Corner.Kicks.Taken"]
-  overall[,"FK.Pass.Comp.Pct"] <- overall[,"FK.Pass.Comp"]/overall[,"FK.Pass.Att"]
+  overall[,"Big.Chances.per.90"] <- (overall[,"Big.Chances"]/overall$MP)*90
+  overall[,"BC.Conversion.Pct"] <- overall[,"BC.Scored"]/overall[,"Big.Chances"]
   overall[,"Pass.Comp.Pct"] <- overall[,"Pass.Comp"]/overall[,"Pass.Att"]
   overall[,"opPass.Comp.Pct"] <- overall[,"opPass.Comp"]/overall[,"opPass.Att"]
+  overall[,"opPPass.Comp.Pct"] <- overall[,"opPPass.Comp"]/overall[,"opPPass.Att"]
+  overall[,"Pct.opPass.Pressed"] <- overall[,"opPPass.Att"]/overall[,"opPass.Att"]
   overall[,"rFreq.Pass.Fwd"] <- overall[,"fwPass.Att"]/(overall[,"fwPass.Att"]+overall[,"sPass.Att"]+overall[,"bPass.Att"])
   overall[,"rFreq.Pass.Side"] <- overall[,"sPass.Att"]/(overall[,"fwPass.Att"]+overall[,"sPass.Att"]+overall[,"bPass.Att"])
   overall[,"rFreq.Pass.Back"] <- overall[,"bPass.Att"]/(overall[,"fwPass.Att"]+overall[,"sPass.Att"]+overall[,"bPass.Att"])
   overall[,"fwPass.Comp.Pct"] <- overall[,"fwPass.Comp"]/overall[,"fwPass.Att"]
   overall[,"sPass.Comp.Pct"] <- overall[,"sPass.Comp"]/overall[,"sPass.Att"]
   overall[,"bPass.Comp.Pct"] <- overall[,"bPass.Comp"]/overall[,"bPass.Att"]
-  overall[,"Pct.Pass.Pressd"] <- overall[,"PPass.Att"]/overall[,"Pass.Att"]
-  overall[,"PPass.Comp.Pct"] <- overall[,"PPass.Comp"]/overall[,"PPass.Att"]
-  overall[,"Pct.opPass.Pressd"] <- overall[,"opPPass.Att"]/overall[,"opPass.Att"]
-  overall[,"opPPass.Comp.Pct"] <- overall[,"opPPass.Comp"]/overall[,"opPPass.Att"]
-  overall[,"rFreq.PPass.Fwd"] <- overall[,"fwPPass.Att"]/(overall[,"fwPPass.Att"]+overall[,"sPPass.Att"]+overall[,"bPPass.Att"])
-  overall[,"rFreq.PPass.Side"] <- overall[,"sPPass.Att"]/(overall[,"fwPPass.Att"]+overall[,"sPPass.Att"]+overall[,"bPPass.Att"])
-  overall[,"rFreq.PPass.Back"] <- overall[,"bPPass.Att"]/(overall[,"fwPPass.Att"]+overall[,"sPPass.Att"]+overall[,"bPPass.Att"])
-  overall[,"fwPPass.Comp.Pct"] <- overall[,"fwPPass.Comp"]/overall[,"fwPPass.Att"]
-  overall[,"sPPass.Comp.Pct"] <- overall[,"sPPass.Comp"]/overall[,"sPPass.Att"]
-  overall[,"bPPass.Comp.Pct"] <- overall[,"bPPass.Comp"]/overall[,"bPPass.Att"]
+  overall[,"rFreq.A3.Passes"] <- overall[,"A3Pass.Att"]/(overall[,"A3Pass.Att"]+overall[,"M3Pass.Att"]+overall[,"D3Pass.Att"])
+  overall[,"rFreq.M3.Passes"] <- overall[,"M3Pass.Att"]/(overall[,"A3Pass.Att"]+overall[,"M3Pass.Att"]+overall[,"D3Pass.Att"])
+  overall[,"rFreq.D3.Passes"] <- overall[,"D3Pass.Att"]/(overall[,"A3Pass.Att"]+overall[,"M3Pass.Att"]+overall[,"D3Pass.Att"])
+  overall[,"A3Pass.Comp.Pct"] <- overall[,"A3Pass.Comp"]/overall[,"A3Pass.Att"]
+  overall[,"M3Pass.Comp.Pct"] <- overall[,"M3Pass.Comp"]/overall[,"M3Pass.Att"]
+  overall[,"D3Pass.Comp.Pct"] <- overall[,"D3Pass.Comp"]/overall[,"D3Pass.Att"]
+  overall[,"rFreq.A3.opPasses"] <- overall[,"A3opPass.Att"]/(overall[,"A3opPass.Att"]+overall[,"M3opPass.Att"]+overall[,"D3opPass.Att"])
+  overall[,"rFreq.M3.opPasses"] <- overall[,"M3opPass.Att"]/(overall[,"A3opPass.Att"]+overall[,"M3opPass.Att"]+overall[,"D3opPass.Att"])
+  overall[,"rFreq.D3.opPasses"] <- overall[,"D3opPass.Att"]/(overall[,"A3opPass.Att"]+overall[,"M3opPass.Att"]+overall[,"D3opPass.Att"])
+  overall[,"A3opPass.Comp.Pct"] <- overall[,"A3opPass.Comp"]/overall[,"A3opPass.Att"]
+  overall[,"M3opPass.Comp.Pct"] <- overall[,"M3opPass.Comp"]/overall[,"M3opPass.Att"]
+  overall[,"D3opPass.Comp.Pct"] <- overall[,"D3opPass.Comp"]/overall[,"D3opPass.Att"]
   overall[,"rFreq.opPass.Fwd"] <- overall[,"fwopPass.Att"]/(overall[,"fwopPass.Att"]+overall[,"sopPass.Att"]+overall[,"bopPass.Att"])
   overall[,"rFreq.opPass.Side"] <- overall[,"sopPass.Att"]/(overall[,"fwopPass.Att"]+overall[,"sopPass.Att"]+overall[,"bopPass.Att"])
   overall[,"rFreq.opPass.Back"] <- overall[,"bopPass.Att"]/(overall[,"fwopPass.Att"]+overall[,"sopPass.Att"]+overall[,"bopPass.Att"])
   overall[,"fwopPass.Comp.Pct"] <- overall[,"fwopPass.Comp"]/overall[,"fwopPass.Att"]
   overall[,"sopPass.Comp.Pct"] <- overall[,"sopPass.Comp"]/overall[,"sopPass.Att"]
   overall[,"bopPass.Comp.Pct"] <- overall[,"bopPass.Comp"]/overall[,"bopPass.Att"]
+  overall[,"Pct.Passes.Pressed"] <- overall[,"PPass.Att"]/overall[,"Pass.Att"]
+  overall[,"PPass.Comp.Pct"] <- overall[,"PPass.Comp"]/overall[,"PPass.Att"]
+  overall[,"rFreq.PPass.Fwd"] <- overall[,"fwPPass.Att"]/(overall[,"fwPPass.Att"]+overall[,"sPPass.Att"]+overall[,"bPPass.Att"])
+  overall[,"rFreq.PPass.Side"] <- overall[,"sPPass.Att"]/(overall[,"fwPPass.Att"]+overall[,"sPPass.Att"]+overall[,"bPPass.Att"])
+  overall[,"rFreq.PPass.Back"] <- overall[,"bPPass.Att"]/(overall[,"fwPPass.Att"]+overall[,"sPPass.Att"]+overall[,"bPPass.Att"])
+  overall[,"fwPPass.Comp.Pct"] <- overall[,"fwPPass.Comp"]/overall[,"fwPPass.Att"]
+  overall[,"sPPass.Comp.Pct"] <- overall[,"sPPass.Comp"]/overall[,"sPPass.Att"]
+  overall[,"bPPass.Comp.Pct"] <- overall[,"bPPass.Comp"]/overall[,"bPPass.Att"]
+  overall[,"Cross.Att.per.90"] <- (overall[,"Cross.Att"]/overall$MP)*90
+  overall[,"Cross.Att.per.opPass"] <- (overall[,"Cross.Att"]/overall["opPass.Att"])
+  overall[,"Cross.Comp.Pct"] <- overall[,"Cross.Comp"]/overall[,"Cross.Att"]
+  overall[,"Launch.Att.per.90"] <- (overall[,"Launch.Att"]/overall$MP)*90
+  overall[,"Launch.Att.per.Pass"] <- (overall[,"Launch.Att"]/overall["Pass.Att"])
+  overall[,"Launch.Comp.Pct"] <- overall[,"Launch.Comp"]/overall[,"Launch.Att"]
+  overall[,"Through.Att.per.90"] <- (overall[,"Through.Att"]/overall$MP)*90
+  overall[,"Through.Att.per.Pass"] <- (overall[,"Through.Att"]/overall["Pass.Att"])
+  overall[,"Through.Att.per.opPass"] <- (overall[,"Through.Att"]/overall["opPass.Att"])
+  overall[,"Through.Comp.Pct"] <- overall[,"Through.Comp"]/overall[,"Through.Att"]
+  overall[,"Throw.In.Att.per.90"] <- (overall[,"Throw.In.Att"]/overall$MP)*90
+  overall[,"Throw.In.Att.per.Pass"] <- (overall[,"Throw.In.Att"]/overall["Pass.Att"])
+  overall[,"Throw.In.Comp.Pct"] <- overall[,"Throw.In.Comp"]/overall[,"Throw.In.Att"]
+  overall[,"CK.Comp.Pct"] <- overall[,"Corner.Kicks.Completed"]/overall[,"Corner.Kicks.Taken"]
+  overall[,"FK.Pass.Comp.Pct"] <- overall[,"FK.Pass.Comp"]/overall[,"FK.Pass.Att"]
   overall[,"TO.Win.Pct"] <- overall[,"TO.Won"]/overall[,"Take.Ons"]
   overall[,"AD.Win.Pct"] <- overall[,"AD.Won"]/overall[,"Aerial.Duels"]
   overall[,"Int.per.90"] <- (overall[,"Interceptions"]/overall$MP)*90
   overall[,"GperSOG"] <- overall[,"Goals.Allowed"]/overall[,"SOG.Faced"]
   overall[,"GperBCSOG"] <- overall[,"BC.Goals.Allowed"]/overall[,"BC.SOG.Faced"]
-  overall[,"Distribution.Success"] <- overall[,"All.Pass.Comp"]/overall[,"All.Pass.Att"]
-  overall[,"Throw.Comp.Pct"] <- overall[,"Throw.Comp"]/overall[,"Throw.Att"]
-  overall[,"Drop.Kick.Comp.Pct"] <- overall[,"Drop.Kick.Comp"]/overall[,"Drop.Kick.Att"]
-  overall[,"GK.FK.Comp.Pct"] <- overall[,"GK.FK.Comp"]/overall[,"GK.FK.Att"]
+  overall[,"GK.Overall.Pass.Comp.Pct"] <- overall[,"All.Pass.Comp"]/overall[,"All.Pass.Att"]
+  overall[,"GK.Throw.Comp.Pct"] <- overall[,"Throw.Comp"]/overall[,"Throw.Att"]
+  overall[,"GK.Drop.Kick.Comp.Pct"] <- overall[,"Drop.Kick.Comp"]/overall[,"Drop.Kick.Att"]
+  overall[,"GKFK.Comp.Pct"] <- overall[,"GK.FK.Comp"]/overall[,"GK.FK.Att"]
   overall
 }
 
