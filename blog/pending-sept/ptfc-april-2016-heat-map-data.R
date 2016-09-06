749f835167510840b03c7c716ea85f401293a167
amj2012 <alfredom790@gmail.com>
2016-09-05 21:28:55 -0700

    Update heat map data & code

--- blog/pending-sept/ptfc-april-2016-heat-map-data.R
+++ blog/pending-sept/ptfc-april-2016-heat-map-data.R
@@ -1,4 +1,4 @@
-##Used for Tobin Heath's Week 1-3 heat maps, but also applies to all PTFC players from that timespan
+##Used for PTFC's Week 1-3 heat maps, but also applies to all PTFC players from that timespan
 
 source("https://raw.githubusercontent.com/amj2012/wosostats/master/code/version-2/create-heat-maps-data.R")
 
@@ -18,6 +18,25 @@ for (matchSheet in match_list){
   source("https://raw.githubusercontent.com/amj2012/wosostats/master/code/version-2/create-location-stats-table.R")
   stats_list[[length(stats_list)+1]] <- stats
   rm(i, stats, df, matchSheet)
+}
+
+
+#Creates a blank overall table
+d <- do.call("rbind", stats_list)
+d <- d[d[,"Team"]=="PTFC",]
+players <- unique(d[,c("Player", "Team")])
+stats <- as.data.frame(matrix(rep(0, length(names(d))), nrow = 1))
+stats <- stats[-1,]
+names(stats) <- names(d)
+stats$Player <- as.character(stats$Player)
+stats$Team <- as.character(stats$Team)
+overall <- merge(players, stats, by=c("Player", "Team"), all=TRUE)
+#for each row in "overall", gets each column's colSums for that row's "Player"-"Team" combo in d
+x <- 1
+while(x <= nrow(overall)) {
+  sub <- d[d[,"Player"] == overall[x,"Player"] & d[,"Team"] == overall[x,"Team"],]
+  overall[x,3:ncol(overall)] <- colSums(sub[,3:ncol(sub)])
+  x <- x + 1
 }
 
 #Write csv files into your directory
