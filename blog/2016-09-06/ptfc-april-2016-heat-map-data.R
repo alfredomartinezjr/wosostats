c2a36fadce950b07a8d2332f434b5f63653d1114
amj2012 <alfredom790@gmail.com>
2016-09-27 18:45:35 -0700

    Tidied code to create PTFC April 2016 heat map data
    Cut down code to just be sourcing of two R files and three functions

--- blog/2016-09-06/ptfc-april-2016-heat-map-data.R
+++ blog/2016-09-06/ptfc-april-2016-heat-map-data.R
@@ -1,20 +1,13 @@
 ##Used for PTFC's Week 1-3 heat maps, but also applies to all PTFC players from that timespan
 
-source("https://raw.githubusercontent.com/amj2012/wosostats/master/code/version-2/create-heat-maps-data.R")
+source("https://raw.githubusercontent.com/amj2012/wosostats/master/code/version-2/getting-data.R")
+source("https://raw.githubusercontent.com/amj2012/wosostats/master/code/version-2/create-location-stats-table.R")
 
 #Get list of match csv files for PTFC from Week 1-3
-getMatchCsvFiles("nwsl-2016", multi_round = c("week-1", "week-2", "week-3"), team = "PTFC")
+getMatchCsvFiles("nwsl-2016", multi_round = c("week-1", "week-2", "week-3"), team = "PTFC", location_complete = TRUE)
 
 #Create stats table for each match
-match_stat <- "everything"
-stats_list <- vector("list", 0)
-#For every match csv file in match_list, create a stats table
-for (matchSheet in match_list){
-  df <- matchSheet
-  source("https://raw.githubusercontent.com/amj2012/wosostats/master/code/version-2/create-location-stats-table.R")
-  stats_list[[length(stats_list)+1]] <- stats
-  rm(i, stats, df, matchSheet)
-}
+stats_list <- createMultiLocStatsTabs(match_list, "everything")
 
 #Write csv files into your directory
-createcsv()
+writeFiles(stats_list, match_names = match_names, match_stat = "everything")
