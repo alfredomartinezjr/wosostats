aacd5336b66fc398566c90c3612bd8cf8b1fabf3
amj2012 <alfredom790@gmail.com>
2017-02-09 21:24:44 -0800

    Added stats list

--- code/testing/creating-stats-script.R
+++ code/testing/creating-stats-script.R
@@ -6,50 +6,49 @@ library(RCurl)
 # test 2: matchURL <- "https://raw.githubusercontent.com/amj2012/wosostats/master/source/csv/nwsl-2016/nwsl-2016-wnyf-was-042916.csv"
 # test 3 (has big chances & key passes): matchURL <- "https://raw.githubusercontent.com/amj2012/wosostats/master/source/csv/nwsl-2016/nwsl-2016-srfc-sbfc-041716.csv"
 
+if(!exists("online_mode")){
+  source("https://raw.githubusercontent.com/amj2012/wosostats/master/code/testing/creating-stats-base-functions.R")
+  source("https://raw.githubusercontent.com/amj2012/wosostats/master/code/testing/creating-stats-columns.R")
+} else if(exists("online_mode") && online_mode == "offline"){
+  source("~/wosostats/code/testing/creating-stats-base-functions.R")
+  source("~/wosostats/code/testing/creating-stats-base-columns")
+}
+
 if(!exists("match_sheet")){
   match_sheet <- getURL(matchURL)
   match_sheet <- read.csv(textConnection(match_sheet), stringsAsFactors = FALSE)
 }
 
- all_players <- createPlayersColumns()
- all_shots <- createShotsColumns(type="legacy")
- all_stats <- merge(all_players, all_shots, by=c("Player","Team","Number"), all.x=TRUE)
- all_keyPasses <- createkeyPassesColumns()
- all_stats <- merge(all_stats, all_keyPasses, by=c("Player","Team","Number"), all.x=TRUE)
- all_bigChances <- createChancesColumns()
- all_stats <- merge(all_stats, all_bigChances, by=c("Player","Team","Number"), all.x=TRUE)
- all_passes <- createPassingColumns()
- all_stats <- merge(all_stats, all_passes, by=c("Player","Team","Number"), all.x=TRUE)
- all_passesByDirection <- createPassingDirectionColumns()
- all_stats <- merge(all_stats, all_passesByDirection, by=c("Player","Team","Number"), all.x=TRUE)
- all_passesByLocation <- createPassingLocationColumns()
- all_stats <- merge(all_stats, all_passesByLocation, by=c("Player","Team","Number"), all.x=TRUE)
- all_passesByRange <- createPassRangeColumns()
- all_stats <- merge(all_stats, all_passesByRange, by=c("Player","Team","Number"), all.x=TRUE)
- all_specialPasses <- createSpecialPassColumns()
- all_stats <- merge(all_stats, all_specialPasses, by=c("Player","Team","Number"), all.x=TRUE)
- all_setPieces <- createSetPieceColumns()
- all_stats <- merge(all_stats, all_setPieces, by=c("Player","Team","Number"), all.x=TRUE)
- all_possessions <- createPossessionColumns()
- all_stats <- merge(all_stats, all_possessions, by=c("Player","Team","Number"), all.x=TRUE)
- all_recoveries <- createRecoveryColumns()
- all_stats <- merge(all_stats, all_recoveries, by=c("Player","Team","Number"), all.x=TRUE)
- all_aerialDuels <- createAerialColumns()
- all_stats <- merge(all_stats, all_aerialDuels, by=c("Player","Team","Number"), all.x=TRUE)
- all_tackles <- createTacklesColumns()
- all_stats <- merge(all_stats, all_tackles, by=c("Player","Team","Number"), all.x=TRUE)
- all_balldisrupt <- createBallDisruptionColumns()
- all_stats <- merge(all_stats, all_balldisrupt, by=c("Player","Team","Number"), all.x=TRUE)
- all_gkshots <- createGkSogColumns()
- all_stats <- merge(all_stats, all_gkshots, by=c("Player","Team","Number"), all.x=TRUE)
- all_highballs <- createGkHighBallColumns()
- all_stats <- merge(all_stats, all_highballs, by=c("Player","Team","Number"), all.x=TRUE)
- all_smothers <- createGkSmotherColumns()
- all_stats <- merge(all_stats, all_smothers, by=c("Player","Team","Number"), all.x=TRUE)
-all_gkPasses <- createGkDistColumns()
-all_stats <- merge(all_stats, all_gkPasses, by=c("Player","Team","Number"), all.x=TRUE)
+stats_list <- list()
+stats_list[[1]] <-  createPlayersColumns()
+stats_list[[2]] <- createShotsColumns()
+stats_list[[3]] <- createKeyPassesColumns() #this takes too long
+stats_list[[4]] <- createChancesColumns() 
+stats_list[[5]] <- createPassingColumns() #this takes too long
+stats_list[[6]] <- createPassingDirectionColumns() #this takes too long
+stats_list[[7]] <- createPassingLocationColumns() #this takes too long
+stats_list[[8]] <- createPassRangeColumns()
+stats_list[[9]] <- createSpecialPassColumns() #this takes too long
+stats_list[[10]] <- createSetPieceColumns() #this takes too long
+stats_list[[11]] <- createPossessionColumns()
+stats_list[[12]] <- createRecoveryColumns()
+stats_list[[13]] <- createAerialColumns()
+stats_list[[14]] <- createTacklesColumns()
+stats_list[[15]] <- createBallDisruptionColumns()
+stats_list[[16]] <- createGkSogColumns()
+stats_list[[17]] <- createGkHighBallColumns()
+stats_list[[18]] <- createGkSmotherColumns()
+stats_list[[19]] <- createGkDistColumns()
+
+for(index in 1:length(stats_list)) {
+  if(exists("match_stats")) {
+    match_stats <- merge(match_stats, stats_list[[index]], by=c("Player","Team","Number"), all.x=TRUE)
+  } else {
+    match_stats <- stats_list[[index]]
+  }
+}
 
 
 #CLEANING UP TABLE----------
-all_stats[is.na(all_stats)] <- 0
-names(all_stats) <- gsub(" ",".", names(all_stats))
+match_stats[is.na(match_stats)] <- 0
+names(match_stats) <- gsub(" ",".", names(match_stats))
