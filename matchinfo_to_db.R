d9bc0f99821aa062db0c6f455c20a846b68a821d
alfredomartinezjr <alfredom790@gmail.com>
2020-05-06 10:02:49 -0700

    Updated match_id column name

--- matchinfo_to_db.R
+++ matchinfo_to_db.R
@@ -8,6 +8,6 @@ dat <- select(dat, -c("year", "month", "day", "home_team",
                       "away_team", "rmd_link", "match_csv_link", 
                       "stats_csv_link", "home_FIFA_rank", 
                       "away_FIFA_rank"))
-matchid <- c(100001:(100001+nrow(dat)-1))
-dat <- cbind(matchid, dat)
+match_id <- c(100001:(100001+nrow(dat)-1))
+dat <- cbind(match_id, dat)
 write_csv(dat, "source/temp_database/matches.csv", na="")
