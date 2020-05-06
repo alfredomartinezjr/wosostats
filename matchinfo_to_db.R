ca45329c5c2b90c8f9a69851d2e08bf6e05dfc75
alfredomartinezjr <alfredom790@gmail.com>
2020-05-06 09:55:14 -0700

    Creates a table with info for all matches
    A table with information for every match in the WoSo Stats database. Will eventually replace the database.csv file.

---
+++ matchinfo_to_db.R
@@ -0,0 +1,13 @@
+library(readr)
+library(dplyr)
+
+fpath <- "database.csv"
+dat <- read_csv(fpath)
+names(dat) <- gsub("\\.","_",names(dat))
+dat <- select(dat, -c("year", "month", "day", "home_team",
+                      "away_team", "rmd_link", "match_csv_link", 
+                      "stats_csv_link", "home_FIFA_rank", 
+                      "away_FIFA_rank"))
+matchid <- c(100001:(100001+nrow(dat)-1))
+dat <- cbind(matchid, dat)
+write_csv(dat, "source/temp_database/matches.csv", na="")
